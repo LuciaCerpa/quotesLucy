d09719481839a9c4e40dab7109cc61765c654e54
LuciaCerpa <luciacerpap@gmail.com>
2022-04-29 18:52:19 -0500

    quotes app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import phrases from './quotes.json';
+import {useState} from 'react';
+const colors = ["red","blue","rgb(237, 237, 19)","green","coral", "violet", "pink", "brown", "orange", "rgb(177, 187, 50)"]
 function App() {
+
+  const color = Math.floor(Math.random() * colors.length)
+
+  const [phrase, setPhrase] = useState(Math.floor(Math.random() * phrases.length))
+
+  const changePhrase = () => {
+      setPhrase(Math.floor(Math.random() * phrases.length))      
+  }
+ 
+ document.body.style=`background:${colors[color]}`
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <blockquote className="sectionPhrases">
+      <p style={{color:colors[color]}}><i class="fa-solid fa-quote-left"></i> {phrases[phrase].quote} <i class="fa-solid fa-quote-right"></i></p>
+        <span style={{color:colors[color]}}>{phrases[phrase].author}</span>
+        <button onClick={changePhrase} style={{background:colors[color]}}>Change Phrase</button>
+      </blockquote>
     </div>
   );
 }
